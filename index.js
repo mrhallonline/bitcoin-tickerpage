6905357e9485e85b15718996ae87eee77018e0cd
Kevin <mrhallonline@gmail.com>
2019-10-16 13:39:30 -0500

    completed get for currency data

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 //jshint esversion: 6
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const request = require("request");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
 
+});
+
+app.post("/", function(req, res){
+// console.log(req.body.crypto);
+request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
+
+console.log(response.body);
+})
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000.");
